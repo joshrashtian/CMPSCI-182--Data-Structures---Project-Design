edb255ac643f85f3965323b5c83dcada743fca3d
joshrashtian <joshrashtian1@gmail.com>
2024-02-07 12:53:10 -0800

    Initial commit

---
+++ Appointment.java
@@ -0,0 +1,8 @@
+public class Appointment {
+    private String month;
+    private int day;
+    private int hour;
+    private int minute;
+    private int year;
+
+}
---
+++ UserInput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static getInt(int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static char getChar(char min, char max) {
+        Scanner scanner = new Scanner(System.in);
+        return
+    }
+
+    public static double getDouble(double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+    }
+
+    public static String getString(int lengthMin, int lengthMax) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
