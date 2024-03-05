0a34179b5dd82439a47b4ae6f06e7b3436e57ff5
joshrashtian <joshrashtian1@gmail.com>
2024-03-05 11:03:55 -0800

    Fixed the Min Char

--- Proj2/MinChar.java
+++ Proj2/MinChar.java
@@ -3,13 +3,20 @@ package Proj2;
 public class MinChar {
     public static char minChar(String str) {
 
-        char min = str.charAt(0);
+        char min = getMin(str, str.charAt(0), 0);
 
-        for (int i = 0; i < str.length(); i++) {
-            if ((int) str.charAt(i) < (int) min) {
-                min = str.charAt(i);
-            }
-        }
         return min;
+    }
+
+    private static char getMin(String min, char currentMin, int index) {
+        if (index < min.length()) {
+            if ((int) min.charAt(index) < (int) currentMin) {
+                return getMin(min, min.charAt(index), index + 1);
+            } else {
+                return getMin(min, currentMin, index + 1);
+            }
+        } else {
+            return currentMin;
+        }
     }
 }
